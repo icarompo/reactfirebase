545ae94af2433e0aa6f9edd3bf255c74f648a90e
icarompo <icaromesquitaponce@outlook.com>
2023-05-22 10:05:08 -0300

    adicionando modelo typescript

---
+++ src/assets/ts/MainComponents.tsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+console.log("MainComponents.tsx is running...");
+
+export function Element() {
+    const [count, setCount] = useState(0);
+    
+
+    return (
+        <>
+        </>
+    );
+}
+
