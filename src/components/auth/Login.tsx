0ee05dced00039cf1fb58d86dbc6efbfe323c4e5
icarompo <icaromesquitaponce@outlook.com>
2023-06-14 17:57:14 -0300

    minimal changes on login page

--- src/components/auth/Login.tsx
+++ src/components/auth/Login.tsx
@@ -3,8 +3,6 @@ import { auth } from '../../api/firebase-config';
 import { signInWithEmailAndPassword } from 'firebase/auth';
 import { useNavigate } from 'react-router-dom'; 
 import './styles.css';
-import axios from 'axios';
-
 interface LoginProps {
   onSuccess: () => void;
 }
@@ -16,28 +14,17 @@ const Login = ({onSuccess}: LoginProps) => {
 
   const login = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-
     if (!(email.trim() === '' || password.trim() === '')) {
-      try {
-        const userCredential = await signInWithEmailAndPassword(auth, email, password);
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
         console.log(userCredential);
         onSuccess();
         navigate('/');
-
-        // Envia o Refresh Token para o servidor para definir o cookie
-        const idToken = await userCredential.user.getIdToken(/* forceRefresh */ true);
-        axios
-          .post('/set-refresh-token', { refreshToken: idToken })
-          .then(() => {
-            console.log('Refresh Token definido como cookie HttpOnly.');
-          })
-          .catch((error) => {
-            console.log('Erro ao definir o Refresh Token como cookie:', error);
-          });
-      } catch (error) {
+      }).catch((error) => {
         console.log(error);
-        alert('Email ou senha incorretos');
+        alert('Email ou senha incorretos ');
       }
+      );
     } else {
       alert('Preencha todos os campos');
     }
