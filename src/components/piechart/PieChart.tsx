3f549ba37ec73c5fb805f0dc4227db1c380a7eb7
icarompo <icaromesquitaponce@outlook.com>
2023-07-21 13:03:45 -0300

    pieChart to panel

---
+++ src/components/piechart/PieChart.tsx
@@ -0,0 +1,70 @@
+interface PieChartData {
+    label: string;
+    value: number;
+    color: string;
+  }
+  
+  interface PieChartProps {
+    data: PieChartData[];
+    radius: number;
+    strokeWidth: number;
+  }
+  
+  export default function PieChart({ data, radius, strokeWidth }: PieChartProps) {
+    const calculateAngle = (value: number, total: number) => (value / total) * 360;
+  
+    const createSlicePath = (
+      startAngle: number,
+      endAngle: number,
+      cx: number,
+      cy: number,
+      r: number,
+      strokeWidth: number
+    ) => {
+      const x1 = cx + r * Math.cos((startAngle * Math.PI) / 180);
+      const y1 = cy + r * Math.sin((startAngle * Math.PI) / 180);
+      const x2 = cx + r * Math.cos((endAngle * Math.PI) / 180);
+      const y2 = cy + r * Math.sin((endAngle * Math.PI) / 180);
+      const largeArcFlag = endAngle - startAngle <= 180 ? "0" : "1";
+  
+      return `
+        M ${cx} ${cy}
+        L ${x1} ${y1}
+        A ${r} ${r} 0 ${largeArcFlag} 1 ${x2} ${y2}
+        Z
+      `;
+    };
+  
+    const total = data.reduce((sum, item) => sum + item.value, 0);
+  
+    let startAngle = 0;
+    const slices = data.map((item, index) => {
+      const angle = calculateAngle(item.value, total);
+      const endAngle = startAngle + angle;
+      const slicePath = createSlicePath(
+        startAngle,
+        endAngle,
+        radius,
+        radius,
+        radius,
+        strokeWidth
+      );
+      startAngle = endAngle;
+  
+      return (
+        <path
+          key={index}
+          d={slicePath}
+          fill={item.color}
+          stroke="#fff"
+          strokeWidth={strokeWidth}
+        />
+      );
+    });
+  
+    return (
+      <svg width={radius * 2} height={radius * 2} viewBox={`0 0 ${radius * 2} ${radius * 2}`}>
+        {slices}
+      </svg>
+    );
+  }
--- src/pages/painel/index.tsx
+++ src/pages/painel/index.tsx
@@ -1,23 +1,52 @@
-import Header from "../../components/header/Header.tsx"; 
+import Header from "../../components/header/Header.tsx";
+import PieChart from "../../components/piechart/PieChart";
+import "./styles.css"
 
 function Page() {
-    return (
-        <>
-        </>
-    );
+
+  interface PieChartData {
+    label: string;
+    value: number;
+    color: string;
+  }
+
+  const data: PieChartData[] = [
+    { label: "Slice 1", value: 130, color: "aqua" },
+    { label: "Slice 2", value: 30, color: "orange" },
+    { label: "Slice 3", value: 10, color: "yellow" },
+    { label: "Slice 4", value: 0, color: "green" },
+    { label: "Slice 5", value: 0, color: "blue" },
+    { label: "Slice 6", value: 0, color: "yellow" },
+  ];
+
+  const pieChartRadius: number = 100;
+  const strokeWidth: number = 3;
+ 
+
+  return (
+    <>
+      <div>
+      <PieChart data={data} radius={pieChartRadius} strokeWidth={strokeWidth} />
+    </div>
+    </>
+  );
 }
 
 interface PainelProps {
-    onLogOut: () => void;
+  onLogOut: () => void;
 }
 
-function Painel({ onLogOut } : PainelProps): JSX.Element {
-    return (
-        <>
-            <Header title="Controle E-Contas" subtitle="Painel" onLogOut={onLogOut}/>
-            <Page />
-        </>
-    );
+function Painel({ onLogOut }: PainelProps): JSX.Element {
+  return (
+    <>
+      <Header
+        title="Controle E-Contas"
+        subtitle="Painel"
+        onLogOut={onLogOut}
+      />
+      <Page />
+    </>
+  );
 }
 
 export default Painel;
