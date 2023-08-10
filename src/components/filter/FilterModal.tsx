094d9ef39f4a541b95744ad665b8970781b261ae
icarompo <icaromesquitaponce@outlook.com>
2023-08-10 10:57:53 -0300

    full filter modal style

--- src/components/filter/FilterModal.tsx
+++ src/components/filter/FilterModal.tsx
@@ -2,6 +2,7 @@ import ReactModal from "react-modal";
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import { useState } from "react";
 import "./form/modal.styles.css";
+import "./filter.styles.css";
 
 function FilterProcessModal({
   isOpen,
@@ -33,19 +34,119 @@ function FilterProcessModal({
           <div className="modal-body">
             <div className="input-section">
               <div className="search">
-                <label htmlFor="search">Pesquisar</label>
-                <select className="select-field" id="search-field">
-                  <option value="relatoria">Relatoria</option>
-                  <option value="assessor">Assessor</option>
-                  <option value="entidade">Entidade</option>
-                  <option value="vinculado">Vinculado</option>
-                  <option value="conselheiro">Conselheiro</option>
-                </select>
-                <input type="text" id="search" />
+                <label className="label" htmlFor="search">
+                  Pesquisar
+                </label>
+                <div className="input">
+                  <select className="input-form" id="search-select">
+                    <option value="relatoria">Relatoria</option>
+                    <option value="assessor">Assessor</option>
+                    <option value="entidade">Entidade</option>
+                    <option value="vinculado">Vinculado</option>
+                    <option value="conselheiro">Conselheiro</option>
+                  </select>
+                  <input className="input-form" type="text" id="search-input" />
+                </div>
+                <div className="line"></div>
               </div>
-              <div className="order"></div>
+              <div className="order">
+                <label className="label" htmlFor="search">
+                  Ordem
+                </label>
+                <div className="input">
+                  <select className="input-form" id="order-definition">
+                    <option value="relatoria">Relatoria</option>
+                    <option value="assessor">Assessor</option>
+                    <option value="entidade">Entidade</option>
+                    <option value="vinculado">Vinculado</option>
+                    <option value="conselheiro">Conselheiro</option>
+                  </select>
+                  <select className="input-form" id="order-filter">
+                    <option value="asc">Crescente</option>
+                    <option value="desc">Decrescente</option>
+                  </select>
+                  <button className="button" id="button" type="button">
+                    +
+                  </button>
+                </div>
+                <div className="order-selected"></div>
+              </div>
             </div>
-            <div className="field"></div>
+            <div className="field">
+              <label className="label" htmlFor="search">
+                Campos
+              </label>
+              <div className="checkList">
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true} />
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+                <div>
+                  <input type="checkbox" name="relatoria" value="relatoria" checked={true}/>
+                  <label htmlFor="relatoria">Relatoria</label>
+                </div>
+              </div>
+              <div className="button-field">
+                <button className="button" id="filter-button" type="button">
+                  Redefinir
+                </button>
+                <button className="button" id="filter-button" type="button">
+                  Aplicar
+                </button>
+              </div>
+            </div>
           </div>
         </ReactModal>
       )}
