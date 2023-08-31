6ba04ec253e95d94d79c62b133b1328a96f7f6d6
Ícaro Mesquita Ponce <130798813+icarompo@users.noreply.github.com>
2023-08-31 10:16:25 -0300

    Delete firebase-algolia-config.ts

--- src/api/firebase-algolia-config.ts
+++
@@ -1,3 +0,0 @@
-import algoliasearch from 'algoliasearch';
-const client = algoliasearch('HSOIS8V3M5', '72244989a3a523ba7aeaca8d4831eece');
-export const algolia_index = client.initIndex('processos');
