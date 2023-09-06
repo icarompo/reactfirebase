a214c609f643b9f11220fbf2d27cc8273293da8e
icarompo <icaromesquitaponce@outlook.com>
2023-09-06 16:10:35 -0300

    changes and navigation menu switch

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -10,18 +10,18 @@ type LayoutProps = {
 
 const Layout = (props: LayoutProps) => {
   return (
-    <div className="app">
-      <div className="app_navigation">
-        <Navigation />
-      </div>
-      <div className="app-content">
-        <div className="app-header">
-          <Header pageName={props.pageName} />
-        </div>
-        {props.children}
-      </div>
+<div className="app">
+  <div className="app_navigation">
+    <Navigation />
+  </div>
+  <div className="app-content">
+    <div className="app-header">
+      <Header pageName={props.pageName} />
     </div>
-  );
+    {props.children}
+  </div>
+</div>
+  )
 };
 
 export default Layout;
--- src/components/navigation/Navigation.tsx
+++ src/components/navigation/Navigation.tsx
@@ -5,46 +5,59 @@ import PersonIcon from "@mui/icons-material/Person";
 import DashboardIcon from "@mui/icons-material/Dashboard";
 import TableRowsIcon from "@mui/icons-material/TableRows";
 import LibraryAddCheckIcon from "@mui/icons-material/LibraryAddCheck";
+import ArrowBackIosSharpIcon from '@mui/icons-material/ArrowBackIosSharp';
+import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
+import { useState } from "react";
 
 function Navigation() {
+  const [showText, setShowText] = useState(true);
+  const navigationItems = [
+    { to: "/", icon: <HomeIcon className="navigation-icon" />, label: "Home" },
+    { to: "/pessoal", icon: <PersonIcon className="navigation-icon" />, label: "Pessoal" },
+    { to: "/processos", icon: <TableRowsIcon className="navigation-icon" />, label: "Processos" },
+    { to: "/painel", icon: <DashboardIcon className="navigation-icon" />, label: "Painel" },
+    { to: "/checagem", icon: <LibraryAddCheckIcon className="navigation-icon" />, label: "Checagem" },
+  ];
+
+  const toggleText = () => {
+    setShowText(!showText);
+  };
+
   return (
-    <>
-      <div className="navigation">
-        <div className="menu-header">
-          <img
-            src="https://irbcontas.org.br/wp-content/uploads/2020/03/associado-titular-logo-tce-to.png"
-            alt="Logo do TCE-TO"
-            className="logo"
-          />
-          <div className="menu-header-text">
-            <h3>Controle </h3>
-            <h3>E-Contas</h3>
-          </div>
-        </div>
-        <div className="links">
-          <Link to="/" className="link">
-            <HomeIcon className="navigation-icon" />
-            Home
-          </Link>
-          <Link to="/pessoal" className="link">
-            <PersonIcon className="navigation-icon" />
-            Pessoal
-          </Link>
-          <Link to="/processos" className="link">
-            <TableRowsIcon className="navigation-icon" />
-            Processos
-          </Link>
-          <Link to="/painel" className="link">
-            <DashboardIcon className="navigation-icon" />
-            Painel
-          </Link>
-          <Link to="/checagem" className="link">
-            <LibraryAddCheckIcon className="navigation-icon" />
-            Checagem
-          </Link>
+    <div className={`navigation ${showText ? "expanded" : "collapsed"}`}>
+      <div className="menu-header">
+        <img
+          src="https://irbcontas.org.br/wp-content/uploads/2020/03/associado-titular-logo-tce-to.png"
+          alt="Logo do TCE-TO"
+          className="logo"
+        />
+        <div className="menu-header-text">
+          {showText && (
+            <>
+              <h3>Controle</h3>
+              <h3>E-Contas</h3>
+            </>
+          )}
         </div>
       </div>
-    </>
+      <div className="menu-links">
+        {navigationItems.map((item, index) => (
+          <Link key={index} to={item.to} className="link">
+            {item.icon}
+            {showText && item.label}
+          </Link>
+        ))}
+      </div>
+      <div className="menu-footer">
+        <button className="toggle-button" onClick={toggleText}>
+          {showText ? (
+            <ArrowBackIosSharpIcon fontSize="small" />
+          ) : (
+            <ArrowForwardIosSharpIcon fontSize="small" />
+          )}
+        </button>
+      </div>
+    </div>
   );
 }
 
