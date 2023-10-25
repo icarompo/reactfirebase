d9848208a89c3873f4de47ec80095743085d759b
icarompo <icaromesquitaponce@outlook.com>
2023-10-25 15:19:35 -0300

    build: instala tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#03467d",
+        secondary: "#1c598a",
+        },
+      },
+    },
+
+  plugins: [],
+};
