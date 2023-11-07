ea75ff348b430f609c9221f78b89cc9465c98a6d
icarompo <icaromesquitaponce@outlook.com>
2023-11-07 17:46:38 -0300

    feat: cria modais para pagina de processos

---
+++ src/pages/03_processos/FilterSection/Modal/AddModal.tsx
@@ -0,0 +1,19 @@
+import { PlusIcon } from "@heroicons/react/24/solid";
+import StyledModal from "../../../../components/Modal";
+import { Tooltip } from "@mui/material";
+
+const AddModal = () => {
+  return (
+    <StyledModal
+      icon={
+        <Tooltip title="Adicionar Processo">
+          <PlusIcon className="h-6 w-6 text-gray-300 hover:text-white hover:scale-110" />
+        </Tooltip>
+      }
+    >
+      <div className="">Opa</div>
+    </StyledModal>
+  );
+};
+
+export default AddModal;
