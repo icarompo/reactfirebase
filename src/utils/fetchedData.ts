d8271a8059c205a21f1f92425ff1e6a0ae27ca25
icarompo <icaromesquitaponce@outlook.com>
2023-11-08 16:37:20 -0300

    fix: remove erros localstorage

--- src/utils/fetchedData.ts
+++ src/utils/fetchedData.ts
@@ -1,21 +1,9 @@
-import { collection, query, getDocs, onSnapshot } from "firebase/firestore";
+import { collection, query, getDocs  } from "firebase/firestore";
 import { userType, procType } from "../App";
 import { db } from "../api/firebase-config";
 
-const LOCAL_STORAGE_KEY_USERS = "usersData";
-const LOCAL_STORAGE_KEY_PROC = "procData";
-
 export const fetchUserData = async () => {
   try {
-    // Verifica se há dados no localStorage
-    const localStorageData = localStorage.getItem(LOCAL_STORAGE_KEY_USERS);
-    if (localStorageData) {
-      const parsedData: Array<userType> = JSON.parse(localStorageData);
-      console.log("Users data fetched from localStorage");
-      return parsedData;
-    } else {
-
-    // Se não houver dados no localStorage, busca no Firestore
     const usersRef = collection(db, "colaboradores");
     const userQuery = query(usersRef);
     const querySnapshot = await getDocs(userQuery);
@@ -30,29 +18,14 @@ export const fetchUserData = async () => {
       } as userType;
       fetchedData.push(userData);
     });
-
-    // Salva os dados no localStorage
-    localStorage.setItem(LOCAL_STORAGE_KEY_USERS, JSON.stringify(fetchedData));
-
     return fetchedData;
-  }
   } catch (error) {
     console.log(error);
   }
 };
 
 export const fetchProcData = async () => {
-  let cont =0;
   try {
-    // Verifica se há dados no localStorage
-    const localStorageData = localStorage.getItem(LOCAL_STORAGE_KEY_PROC);
-    if (localStorageData) {
-      const parsedData: Array<procType> = JSON.parse(localStorageData);
-      cont = cont + 1;
-      console.log(cont);
-      return parsedData;
-    } else {
-    // Se não houver dados no localStorage, busca no Firestore
     const dataCollectionRef = collection(db, "dados");
     const procQuery = query(dataCollectionRef);
     const querySnapshot = await getDocs(procQuery);
@@ -62,43 +35,9 @@ export const fetchProcData = async () => {
       fetchedData.push({ id: doc.id, ...rest });
     });
 
-    // Salva os dados no localStorage
-    localStorage.setItem(LOCAL_STORAGE_KEY_PROC, JSON.stringify(fetchedData));
-
     console.log("Process data fetched from Firestore");
     return fetchedData;
-  }
   } catch (error) {
     console.log(error);
   }
-};
-
-// Função para monitorar alterações no Firestore e atualizar o localStorage
-export const listenForFirestoreChanges = () => {
-  const usersRef = collection(db, "colaboradores");
-  const dataCollectionRef = collection(db, "dados");
-
-  // Monitorar alterações em "colaboradores"
-  onSnapshot(usersRef, (snapshot) => {
-    snapshot.docChanges().forEach((change) => {
-      if (change.type === "added" || change.type === "modified" || change.type === "removed") {
-        // Buscar e atualizar os dados no localStorage
-        fetchUserData().then((userData) => {
-          localStorage.setItem(LOCAL_STORAGE_KEY_USERS, JSON.stringify(userData));
-        });
-      }
-    });
-  });
-
-  // Monitorar alterações em "dados"
-  onSnapshot(dataCollectionRef, (snapshot) => {
-    snapshot.docChanges().forEach((change) => {
-      if (change.type === "added" || change.type === "modified" || change.type === "removed") {
-        // Buscar e atualizar os dados no localStorage
-        fetchProcData().then((procData) => {
-          localStorage.setItem(LOCAL_STORAGE_KEY_PROC, JSON.stringify(procData));
-        });
-      }
-    });
-  });
 };
